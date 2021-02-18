9da3396ed22efd3e1b520edbff64fd006395f1db
Henryk Budzinski <henrykzfb@gmail.com>
2021-02-17 23:29:05 -0300

    Adicionaro o projeto da Api1

---
+++ Api1/Controllers/TaxaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaController : ControllerBase
+    {
+        // private readonly ILogger<TaxaController> _logger;
+        //
+        // public TaxaController(ILogger<TaxaController> logger)
+        // {
+        //     _logger = logger;
+        // }
+        
+        [HttpGet]
+        [Route("/taxaJuros")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<decimal>> Get()
+        {
+            return await Task.Run(() => 0.01M);
+        }
+    }
+}
