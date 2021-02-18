55167cea9afcca00249c4b4db12e95ada376f945
Henryk Budzinski <henrykzfb@gmail.com>
2021-02-18 04:27:29 -0300

    Implementado endpoint que retorna endereço do repositório

---
+++ Api2/Controllers/GithubController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Api2.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class GithubController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        
+        public GithubController(IConfiguration config)
+        {
+            _config = config;
+        }
+        
+        [HttpGet]
+        [Route("/showmethecode")]
+        public async Task<string> GetUrl()
+        {
+            return await Task.Run(() => _config.GetValue<string>("urls:github"));
+        }
+    }
+}
